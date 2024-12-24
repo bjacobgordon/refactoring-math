bjacobgordon/state-of-math

wip - feature: adds support for Quantity hyperoperation at level 2

NumberSystems/Sources/NumberSystems/Quantity.swift
@@ -4,7 +4,8 @@ public struct Quantity: Sendable {
     fileprivate static let standardEmbodyingElement = tallyMark
     private static let             embodimentOfNone = ""
     
-    public static let none = Self(Self.embodimentOfNone)!
+    public static let none     = Self(Self.embodimentOfNone)!
+    public static let singular = Self(String(Self.standardEmbodyingElement))!
     
     fileprivate var embodiment: String
     
@@ -46,6 +47,14 @@ extension Quantity: Hyperoperable {
         self.embodiment.removeLast()
     }
     
+    public static func identity(at givenLevel: Quantity) -> Quantity? {
+        switch givenLevel {
+        case Quantity.none    : return nil
+        case Quantity.singular: return Quantity.none
+        default               : return Quantity.singular
+        }
+    }
+    
     public static func hyperoperateUpon(
         atLevel givenLevel: Quantity,
         by givenOperametrum: Quantity,
@@ -58,6 +67,18 @@ extension Quantity: Hyperoperable {
             givenOperametrum.embodiment.forEach { _ in
                 Self.hyperoperateUpon(atLevel: givenLevel.predecessor, by: givenOperand, on: &givenOperand)
             }
+            
+            return
+        }
+        else if givenLevel == Quantity("||")! {
+            let precedingLevel = givenLevel.predecessor
+            var operatum = Self.identity(at: precedingLevel)!
+            
+            givenOperametrum.embodiment.forEach { _ in
+                Self.hyperoperateUpon(atLevel: precedingLevel, by: givenOperand, on: &operatum)
+            }
+            
+            givenOperand = operatum
             
             return
         }

NumberSystems/Tests/NumberSystemsTests/QuantityTests.swift
@@ -98,3 +98,20 @@ func hyperoperationAtLevel0(_ givenOperametrum: Quantity) async throws {
     let twelve = ten.hyperoperatedUpon(atLevel: one, by: two)
     #expect(12.represents(twelve))
 }
+
+@Test("Comparison of level 2 hyperoperation to multiplication", arguments: [
+    (   1, 1),
+    (  10, 2),
+    ( 100, 3),
+    (1000, 4)
+])
+func hyperoperationAtLevel2(_ givenMultiplicand: Int, _ givenMultiplier: Int) async throws {
+    let secondLevel = 2.asQuantity
+    
+    let operametrum = givenMultiplier  .asQuantity
+    let operand     = givenMultiplicand.asQuantity
+    let operatum    = operand.hyperoperatedUpon(atLevel: secondLevel, by: operametrum)
+    
+    let product = givenMultiplicand * givenMultiplier
+    #expect(product.represents(operatum))
+}

bjacobgordon/refactoring-math

NumberSystems/Sources/NumberSystems/Quantity.swift
@@ -47,6 +47,14 @@ extension Quantity: Hyperoperable {
         self.embodiment.removeLast()
     }
     
+    public static func identity(at givenLevel: Quantity) -> Quantity? {
+        switch givenLevel {
+        case Quantity.none    : return nil
+        case Quantity.singular: return Quantity.none
+        default               : return Quantity.singular
+        }
+    }
+    
     public static func hyperoperate(
         at givenLevel      : Quantity,
         by givenOperametrum: Quantity,
@@ -71,6 +79,19 @@ extension Quantity: Hyperoperable {
             
             effectiveOperametrum.embodiment.forEach { _ in
                 runningOperatum.succeed()
+            }
+            
+            return runningOperatum
+        }
+        else
+        if (givenLevel == Quantity("||")!) {
+            let precedingLevel = givenLevel.predecessor
+            var runningOperatum = Self.identity(at: precedingLevel)!
+            
+            effectiveOperametrum.embodiment.forEach { _ in
+                let eachOperand     = runningOperatum
+                let eachOperametrum =   givenOperand
+                runningOperatum     = Self.hyperoperate(at: precedingLevel, by: eachOperametrum, on: eachOperand)
             }
             
             return runningOperatum

NumberSystems/Tests/NumberSystemsTests/QuantityTests.swift
@@ -106,3 +106,20 @@ func hyperoperationAtLevel1(_ givenAugend: Int, _ givenAddend: Int) async throws
     let sum = givenAugend + givenAddend
     #expect(sum.represents(operatum))
 }
+
+@Test("Comparison of level 2 hyperoperation to multiplication", arguments: [
+    (   1, 1),
+    (  10, 2),
+    ( 100, 3),
+    (1000, 4)
+])
+func hyperoperationAtLevel2(_ givenMultiplicand: Int, _ givenMultiplier: Int) async throws {
+    let secondLevel = 2.asQuantity
+    
+    let operametrum = givenMultiplier  .asQuantity
+    let operand     = givenMultiplicand.asQuantity
+    let operatum    = operand.hyperoperatedUpon(at: secondLevel, by: operametrum)
+    
+    let product = givenMultiplicand * givenMultiplier
+    #expect(product.represents(operatum))
+}